3f5bd5a646cd4361d640c3d5e24aad2992763513
zaidoune <maherzaidoune@gmail.com>
2018-04-18 11:50:46 +0100

    security patch

--- common/models/user.js
+++ common/models/user.js
@@ -2,62 +2,56 @@
 
 var config = require('../../server/config.json');
 var path = require('path');
+var app = require('../../server/server');
 
 
 
 module.exports = function(User) {
-  /*
-  User.remoteMethod('addDoctor',{
-      accepts:[
-      {
-      arg:"doctorId",
-      type:"object",
-      default:{
-        "id":"number"
-      },
-      http:{
-        source:"body"
-      },
-      description:"doctor id",
-      required:"true"},{"arg": "options", "type": "object", "http": "optionsFromRequest"}],
-      description:"add doctor  to user database",
-      http:{path:"/addDoctor/",verb:"post"},
-      returns:{arg:"put",type:"object"}
-    });
-    */
+
+  // add an existing doctor to user
     User.addDoctor  = function(doctorId,options,callback){
-      var app = require('../../server/server');
-            //var db = app.dataSources.db;
-          //  var  doctor = db.connector.collection('Doctor').find({where: {'_id': id}});
-          //  db.connector.collection('User').update(
-          //  { userId: "5acf5a49b14afa24f4f4227c" },
-          //  { $push: { doctors : id }
           const token = options && options.accessToken;
           const userId = token && token.userId;
           User.findOne({where:{id : userId}},function(err,user){
-          //  console.log(userId);
-            app.models.Doctor.find({where: { id: doctorId}}, function(err, doctor) {
+          app.models.Doctor.findOne({where: { id: doctorId}}, function(err, doctor) {
               if(err){
                 console.log(err);
                 throw err
               }
-              //console.log(user.doctors());
-              //console.log(doctor);
-
-              /*User.updateAll(user,user.doctors.add(doctor),function(err,info){
-
-              });
-              */
-              user.doctors().push(user.doctors())
               console.log(user);
-              //user.doctors.add({"fullName":"maher"});
-
-              callback(user.doctors(),doctorId);
-            //  console.log(doctor);
-
+              console.log(doctor);
+              user.doctors.create(doctor)
+              callback(doctor);
              });
           })
-
 }
+
+// add an existing Healthstruct to user
+  User.addHealthStruct  = function(healthStructId,options,callback){
+        const token = options && options.accessToken;
+        const userId = token && token.userId;
+        User.findOne({where:{id : userId}},function(err,user){
+        app.models.healthStruct.findOne({where: { id: healthStructId}}, function(err, h) {
+            if(err){
+              console.log(err);
+              throw err
+            }
+            console.log(user);
+            console.log(h);
+            user.healthStructs.create(h)
+
+
+            callback(h);
+           });
+        })
+}
+
+// remote hook for adding doctor  created via user to doctor  database
+
+ User.observe('*.__post__healthStructs__', function(ctx, inst, next) {
+    console.log("remote methode called");
+    next()
+  });
+
 
 }
